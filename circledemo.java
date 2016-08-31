7dc0bb561745c4028424d5b8b30232343707aedd
Pacurar Ovidiu Paul <pacurarovidiupaul@live.com>
2016-08-31 15:04:23 +0300

    Circle demo!

---
+++ circledemo.java
@@ -0,0 +1,31 @@
+/**
+ * @autor: Ovidiu Pacurar
+ * @description: This program calculate area and circumference of a circle with
+ * the user interaction. User will be prompt to enter the radius and the result
+ * will be calculated base on the provided radius value.
+ */
+
+package ro.testjava.circledemo;
+
+import java.util.Scanner;
+
+public class CircleDemo {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("Enter the radius: ");
+		/*
+		 * We are storing the entered radius in double because a user can enter
+		 * the radius in decimals
+		 */
+		double radius = sc.nextDouble();
+		// Area = PI * radius * radius
+		double area = Math.PI * (radius * radius);
+		System.out.println("The are o circle is: " + area);
+		// Circumferene = 2 * PI * radius
+		double circumference = Math.PI * 2 * radius;
+		System.out.println("The circumference of cirlces is: " + circumference);
+
+	}
+
+}
